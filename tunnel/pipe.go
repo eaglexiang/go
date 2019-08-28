1871a28512a31b2baa53e8b50c0af971d8ff56d1
Eagle <eagle.xiang@outlook.com>
2019-08-28 19:40:38 +0800

    Pipe关闭入口连接前应该检查nil

--- tunnel/pipe.go
+++ tunnel/pipe.go
@@ -4,7 +4,7 @@
  * @Email: eagle.xiang@outlook.com
  * @Github: https://github.com/eaglexiang
  * @Date: 2019-07-24 21:22:45
- * @LastEditTime: 2019-08-28 19:39:06
+ * @LastEditTime: 2019-08-28 19:40:16
  */
 
 package tunnel
@@ -57,7 +57,10 @@ func (p *pipe) Close() {
 	defer p.l.Unlock()
 
 	p.pipeStatus.Close()
-	p.in.Close()
+
+	if p.in != nil {
+		p.in.Close()
+	}
 }
 
 // Closed 是否已经关闭
