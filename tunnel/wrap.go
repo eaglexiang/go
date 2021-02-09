a03395d820a039a591b65efce42c5c8e09b2873f
eaglexiang <eagle.xiang@outlook.com>
2021-02-09 15:39:01 +0800

    tunnel.Flow 超时可配置化

--- tunnel/wrap.go
+++ tunnel/wrap.go
@@ -1,13 +1,22 @@
 package tunnel
 
-import "net"
+import (
+	"net"
+	"time"
+)
 
 // Flow 双向流动
-func Flow(left, right net.Conn) {
+func Flow(left, right net.Conn, timeout ...time.Duration) {
 	t := GetTunnel()
 	t.SetLeft(left)
 	t.SetRight(right)
+
+	if len(timeout) > 0 {
+		t.SetTimeout(timeout[0])
+	}
+
 	t.Flow()
 	t.Close()
+
 	PutTunnel(t)
 }
